67aa4503474eaca9d05c2cfbf71386ae495eed64
Luca Santini <lucasantinidev@gmail.com>
2020-07-15 16:24:12 +0200

    aggiunta funzione category alla classe Post

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = ['id', 'title', 'content', 'slug'];
+
+    public function category() {
+      return $this->belongsTo('App\Category');
+  }
 }
